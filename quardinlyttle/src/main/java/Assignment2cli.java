322d3d2b8dcf3b411d4e1167648d15c5e850805d
Quardin Lyttle <lyttleqd@gmail.com>
2025-07-25 16:11:18 -0400

    Tried creating a maven project as well as did some background reading.

---
+++ quardinlyttle/src/main/java/Assignment2cli.java
@@ -0,0 +1,22 @@
+/*
+Quardin Lyttle Assignment 2
+Client Portion
+ */
+import java.io.*;
+import java.net.*;
+
+public class Assignment2cli {
+
+    public void main(String args){
+    	if(args.length()!= 3) {
+    		System.out.println("Please provide the IP address, port number, and filename!");
+    		System.exit(0);
+    	}
+    	
+    	// https://www.baeldung.com/udp-in-java
+    	int portNum = args[2];
+    	
+        
+    }
+    
+}
